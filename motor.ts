c55945b5ab0e131cd4a78e94ef5b3d99d67cbdb9
ABDULLAHALAMIEN <m-12681062@moe-dl.edu.my>
2025-10-19 08:37:13 +0800

    Update motor.ts

--- motor.ts
+++ motor.ts
@@ -17,38 +17,6 @@ enum MotorDirection {
 };
 
 namespace MakerDrive {
-
-    /**
-     * Brake the motor
-     * @param motor Motor channel. eg: Motor.M1, Motor.M2
-     */
-    //% group="DC Motors"
-    //% weight=20
-    //% blockGap=8
-    //% blockId=MakerDrive_brake_motor
-    //% block="brake motor %motor"
-    export function brakeMotor(motor: MotorChannel): void {
-        switch (motor) {
-            case MotorChannel.M1:
-                MakerDrive.i2cWrite(REG_ADD_M1A, 0);
-                MakerDrive.i2cWrite(REG_ADD_M1B, 0);
-                break;
-
-            case MotorChannel.M2:
-                MakerDrive.i2cWrite(REG_ADD_M2A, 0);
-                MakerDrive.i2cWrite(REG_ADD_M2B, 0);
-                break;
-
-            case MotorChannel.All:
-                MakerDrive.i2cWrite(REG_ADD_M1A, 0);
-                MakerDrive.i2cWrite(REG_ADD_M1B, 0);
-                MakerDrive.i2cWrite(REG_ADD_M2A, 0);
-                MakerDrive.i2cWrite(REG_ADD_M2B, 0);
-                break;
-        }
-    }
-
-
     /**
      * Run the motor forward or backward (Speed = 0-255).
      * @param motor Motor channel.
